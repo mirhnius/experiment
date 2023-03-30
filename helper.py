28ca465dea69a83a3d4630ebe969a2afbdf9a320
mirhnius <niushamirhakimi@gmail.com>
2023-03-30 13:20:57 -0400

    add ICA function

--- helper.py
+++ helper.py
@@ -1,8 +1,10 @@
 from random import sample
+import pathlib
+from nilearn.decomposition import CanICA
 
 def DBM_sample_filename(r):
-    dir_ = "/mnt/out/out_2/vol"
-    #dir_ = "/data/origami/niusha/out/out_s/"
+    # dir_ = "/mnt/out/out_2/vol"
+    dir_ = "/data/origami/niusha/out/out_s/"
     if r>=0 and r<=9:
         return dir_ + "000" + str(r) + ".nii.gz"
     elif r>=10 and r<=99:
@@ -10,8 +12,29 @@ def DBM_sample_filename(r):
     else:
         return dir_ + "0" + str(r) + ".nii.gz"
 
-def write_samples_filename(filename, ID_list):    
-    f = open(filename, "w")
-    for id in ID_list:
-        f.write(DBM_sample_filename(id)+ "\n")
+def filename_list(IDS):
+    filenames = []
+    for s in IDS:
+        filenames.append(DBM_sample_filename(s))
+    return filenames
+
+def write_samples_filename(filenames, i):    #rewrite it with pathlib
+   
+    P = pathlib.Path('/data/origami/niusha/out/ICAs') / (f"ICs_{i}")
+    P.mkdir(exist_ok=False)
+    f = open(P / "PD.txt", "w")
+    for name in filenames:
+        f.write(name+ "\n")
     f.close()
+
+def ICA_decomposition(filenames, i):
+    canica = CanICA(n_components=3,
+                memory="nilearn_cache", memory_level=2,
+                verbose=10,
+                mask_strategy='whole-brain-template',
+                random_state=0)
+    canica.fit(filenames)
+
+    P = pathlib.Path('/data/origami/niusha/out/ICAs') / (f"ICs_{i}")
+    canica_components_img = canica.components_img_
+    canica_components_img.to_filename(P / (f'ICAs{i}.nii.gz'))
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
+from helper import *
 
-subject_df = pd.read_csv("/mnt/input/subject_IDs.csv")
-#subject_df = pd.read_csv("/data/origami/niusha/input/subject_IDs.csv")
+#subject_df = pd.read_csv("/mnt/input/subject_IDs.csv")
+subject_df = pd.read_csv("/data/origami/niusha/input/subject_IDs.csv")
 
 
 N = len(subject_df.ID)
@@ -14,21 +15,12 @@ Healthy_subject = subject_df.ID.iloc[Healthy_index]
 PD_index = np.where(subject_df.PD == 1)
 PD_subject = subject_df.ID.iloc[PD_index]
 
-
+from random import sample
 PD_sample = sample((PD_index[0]).tolist(), len(PD_index[0]))
 Healthy_sample = sample((Healthy_index[0]).tolist(), len(Healthy_index[0]))
 
-PD_filenames = DBM_sample_filename(PD_sample)
-Healthy_filenames = DBM_sample_filename(Healthy_sample)
+PD_filenames = filename_list(PD_sample)
 
-from nilearn.decomposition import CanICA
-
-canica = CanICA(n_components=3,
-                memory="nilearn_cache", memory_level=2,
-                verbose=10,
-                mask_strategy='whole-brain-template',
-                random_state=0)
-canica.fit(PD_filenames)
-
-canica_components_img = canica.components_img_
-canica_components_img.to_filename('ICAs.nii.gz')
+# Healthy_filenames = filename_list(Healthy_sample)
+write_samples_filename(PD_filenames, 3)
+ICA_decomposition(PD_filenames, 3)
