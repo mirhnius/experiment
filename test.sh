d1d88e1d28c5418b44782cd161eb2589c5a2ca67
mirhnius <niushamirhakimi@gmail.com>
2023-05-15 15:53:02 -0400

    job submission
    it works for a job but I need to change it to work with an array and also store elese where than code

--- test.sh
+++ test.sh
@@ -9,47 +9,31 @@
 #$ -o $JOB_NAME_$TASK_ID.out
 #$ -e $JOB_NAME_$TASK_ID.err
 
+PARENT_DIR="/data/origami/niusha/code/experiment/test"
+PARENT_DIR_MELODIC="/mnt/code/test"
 
-# INPUTFILES=(/data/origami/niusha/out/README.md)
+INPUTFILES=(/data/origami/niusha/out/test.txt)
 
-# INPUTFILENAME="${INPUTFILES[$SGE_TASK_ID -1]}"
-# DESTINATION_DIR="/data/origami/niusha/code/experiment/test/test-$SGE_TASK_ID"
+INPUTFILENAME="${INPUTFILES[$SGE_TASK_ID -1]}"
+DESTINATION_DIR="${PARENT_DIR}/test-$SGE_TASK_ID"
 
-# if [ ! -d "$DESTINATION_DIR" ]
-# then 
-#     mkdir -p $DESTINATION_DIR
-# fi
+if [ ! -d "$DESTINATION_DIR" ]
+then 
+    mkdir -p $DESTINATION_DIR
+fi
 
+cp $INPUTFILENAME $DESTINATION_DIR
+cd $DESTINATION_DIR
 
-# WORKING_DIR="/data/origami/niusha/code/experiment/test/test-$SGE_TASK_ID-working"
+# #Run the program
+export SGE_TASK_ID
+export PARENT_DIR_MELODIC
 
-# if [ ! -d "$WORKING_DIR" ]
-# then
-#     mkdir -p $WORKING_DIR
-# fi
-
-# cp $INPUTFILENAME $WORKING_DIR
-# cd $WORKING_DIR
-
-# # #Run the program
-# export SGE_TASK_ID
 
 singularity exec --bind /data/origami/niusha/code/experiment:/mnt/code \
 --bind /data/origami/niusha/input:/mnt/input:ro \
+--bind /data/origami/niusha/out:/mnt/out \
 /data/origami/niusha/fsl_python.sif \
 /mnt/code/test_singularity_fsl.sh
 
-echo "this is new"
-
-# OUTPUT = "$INPUT_DIR/output"
-# if [! -d "$OUTPUT"]
-# then
-#     mkdir -p $OUTPUT
-# fi
-
-# exit 
-
-# cd ./output
-
-# cp * $DESTINATION_DIR
-# rm -rf $WORKING_DIR
+# rm ${DESTINATION_DIR}/boot${SGE_TASK_ID}.nii.gz
--- test_singularity_fsl.sh
+++ test_singularity_fsl.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-echo "test_singularity_fsl.sh is running"
-# cd $WORKING_DIR
+# melodic -i /mnt/input/DBM_data.nii -o /mnt/code/test/test-1 -d 30 --Oall 
 
-fslinfo /mnt/input/DBM_data.nii
-echo "test 1"
-melodic -i /mnt/input/DBM_data.nii -o /mnt/code/test/test-1 -d 30 --Oall 
-echo "no melodc"
+WORKING_DIR="${PARENT_DIR_MELODIC}/test-${SGE_TASK_ID}"
+INPUTFILE="${WORKING_DIR}/test.txt"
+
+COMMAND="fslmerge -t ${WORKING_DIR}/boot${SGE_TASK_ID}.nii.gz" 
+while IFS="" read -r FILENAME || [ -n "$FILENAME" ]
+do
+  COMMAND="${COMMAND} ${FILENAME}"
+
+done < $INPUTFILE
+
+eval $COMMAND
+
+melodic -i ${WORKING_DIR}/boot${SGE_TASK_ID}.nii.gz -o ${WORKING_DIR}
